a232e51f656c0adbc1907bdaeffaa2a0b861d956
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 02:56:55 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -23,9 +23,10 @@ class Owner
   end
 
   def pets
-    pets = {:fishes => [], :dogs => [], :cats => []}
+    @pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
-
+  def buy_fish(name)
+    fish = Fish.new()
 
 end
